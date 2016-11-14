36a77e9fb0f386d55010c1f701345e4c985a69ca
Halvor Nafstad <Halvor Nafstad>
2016-11-14 15:15:39 +0100

    New plotfile sameplots.py plots energies in same figure for different temperatures, magnetisation and accepted configurations as well

---
+++ code/sameplots.py
@@ -0,0 +1,94 @@
+from matplotlib.pylab import *
+from numpy import *
+
+filenames = ["T_1.00.txt", "T_2.40.txt"]
+
+arr = False
+color = iter(cm.rainbow(linspace(0, 1, len(filenames))))
+temp = []
+E = []
+for file in filenames:
+	f = open(file)
+	
+	if arr:
+		i=0
+		for line in f:
+			words = line.split()
+
+			E[i] = float(words[1])
+			
+			i+=1
+	else:
+		for line in f:
+			words = line.split()
+			temp.append(float(words[0]))
+			E.append(float(words[1]))
+			arr = True
+
+	temp = array(temp); E = array(E); print len(E)
+
+	plot(temp, E, c = next(color))
+	hold("On")
+	xlabel("Monte Carlo cycles")
+	ylabel(r"$<E>$ $[spin^{-1}]$")
+legend(["T=1.00", "T=2.40"], loc="upper left")
+show()
+
+color = iter(cm.rainbow(linspace(0, 1, len(filenames))))
+M = []
+arr=False
+for file in filenames:
+	f = open(file)
+	
+	if arr:
+		i=0
+		for line in f:
+			words = line.split()
+
+			M[i] = float(words[2])
+			
+			i+=1
+	else:
+		for line in f:
+			words = line.split()
+			M.append(float(words[2]))
+			arr = True
+
+	M = array(M)
+
+	plot(temp, M, c = next(color))
+	hold("On")
+	xlabel("Monte Carlo cycles")
+	ylabel(r"$<|M|>$ $[spin^{-1}]$")
+legend(["T = 1.00", "T = 2.40"], loc="upper left")
+show()
+
+
+color = iter(cm.rainbow(linspace(0, 1, len(filenames))))
+A = []
+arr=False
+for file in filenames:
+	f = open(file)
+	
+	if arr:
+		i=0
+		for line in f:
+			words = line.split()
+
+			A[i] = float(words[3])
+			
+			i+=1
+	else:
+		for line in f:
+			words = line.split()
+			A.append(float(words[3]))
+			arr = True
+
+	A = array(A)
+
+	plot(temp, A, c = next(color))
+	hold("On")
+	xlabel("Monte Carlo cycles")
+	ylabel(r"Accepted configurations")
+legend(["T = 1.00", "T = 2.40"], loc="upper left")
+show()
